40ee4f27524e711acd54490d8b88861b43efa48f
Rafael Correa <111290427+rafaelcorrea-dev@users.noreply.github.com>
2024-05-26 17:05:59 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/get.test.js
+++ tests/integration/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parserUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parserUpdatedAt);
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.opened_connection).toEqual(1);
 });
+
+// test.only("Teste de SQL Injection", async () => {
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//   );
+// });
