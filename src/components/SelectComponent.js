9eeb51d4319e7f07364e2aefae2e66a4e5f1684e
unknown <rafaelcorrea.rc@gmail.com>
2024-06-11 23:43:22 -0300

    feat: adds `InputComponent` and `SelectComponent`

---
+++ src/components/SelectComponent.js
@@ -0,0 +1,11 @@
+export default function SelectComponent({ value, onChange, options }) {
+  return (
+    <select value={value} onChange={onChange}>
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
